444b25aa5048c0e96291e05f62228b38285b6228
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2022-01-06 14:17:40 +0100

    feat(client): Add prompt and command-line options

---
+++ client/client.config.js
@@ -0,0 +1,2 @@
+export const DEFAULT_HOST = '127.0.0.1';
+export const DEFAULT_PORT = 1234;
--- client/src/main.js
+++ client/src/main.js
@@ -1,26 +1,42 @@
 import net from "net";
 import {createUser} from "./ask.js";
+// import {app} from "electron";
+import {Command} from "commander";
+import {DEFAULT_HOST, DEFAULT_PORT} from "../client.config.js";
+const cli = new Command();
 
-const HOST = '127.0.0.1';
-const PORT = 8080;
+cli
+    .version('0.0.1')
+    .option('-p, --port <PORT>', "TCP Socket's port")
+    .option('-a, --host <HOST>', "TCP Socket's address")
 
-const client = new net.Socket();
+cli.parse(process.argv)
+
+const options = cli.opts()
+console.log({options})
 
 const user = await createUser()
 await user.action();
 
-client.connect(PORT, HOST, function() {
+const HOST = options.host ?? DEFAULT_HOST;
+const PORT = options.port ?? DEFAULT_PORT;
+
+
+const client = new net.Socket();
+
+
+client.connect(PORT, HOST, function () {
     console.log('CONNECTED TO: ' + HOST + ':' + PORT);
 });
-let i = 0;
+
 // Add a 'data' event handler for the client socket
 // data is what the server sent to this socket
-client.on('data', function(data) {
+client.on('data', function (data) {
     user.onData(data);
     client.destroy();
 });
 
 // Add a 'close' event handler for the client socket
-client.on('close', function() {
+client.on('close', function () {
     console.log('Connection closed');
 });
